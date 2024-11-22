432cbe916ae7a90652d0d53118e0e160d6f283ca
HSTPL_LAP_335 <hstpl_lap_335@HSTPL-LAP-335s-MacBook-Pro.local>
2024-11-22 12:49:35 +0530

    stage and live url

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,40 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    // CallKit setup
+    initializeCallKitIfNeeded()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Function to check if the locale is China
+  func isChinaLocale() -> Bool {
+    let locale = NSLocale.current
+    if let countryCode = locale.regionCode {
+        return countryCode == "CN" // Country code for China
+    }
+    return false
+  }
+
+  // Initialize CallKit only if the user is not in China
+  func initializeCallKitIfNeeded() {
+    if !isChinaLocale() {
+        setupCallKit()
+    } else {
+        print("CallKit is disabled for China region")
+    }
+  }
+
+  // Placeholder function for CallKit setup
+  func setupCallKit() {
+    print("CallKit functionality is initialized")
+    // Add your CallKit initialization logic here
   }
 }
